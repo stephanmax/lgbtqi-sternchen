624083afcf57636ec54e28237337b9c7b2b89cc9
Steffen Kahl <steffen.kahl@ue-germany.de>
2021-01-09 13:28:54 +0100

    [ADDED] Tilemap1 and PowerupBlocks.svg

---
+++ Tilemaps/Tilemap1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tilemap1" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../Grafiken/Tilemap1.png" width="512" height="512"/>
+</tileset>
